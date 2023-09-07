00d81addec2f310a600974b1081c390436e2f32f
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-09-07 10:39:17 +0000

    Done

--- README.md
+++ README.md
@@ -10,3 +10,19 @@ I successfully created the API, adhered to the specified JSON format, and ensure
 - **Requirements:** Accept two GET request query parameters and return specific JSON data.
 - **Acceptance Criteria:** Ensure endpoint creation, correct response format, and adherence to task requirements.
 
+## Solution
+
+We need to get some out put like 
+
+```json
+{
+    "slack_name": "example_name",
+    "current_day": "Thursday",
+    "utc_time": "2023-09-07T10:03:00.504Z",
+    "track": "backend",
+    "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
+    "github_repo_url": "https://github.com/username/repo",
+    "status_code": 200
+}
+
+```
--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,27 @@ const app = express();
 app.get("/api", (req: Request, res: Response) => {
   const { slack_name, track } = req.query;
 
+  // Validate slack_name and track parameters
+  if (
+    !slack_name ||
+    typeof slack_name !== "string" ||
+    slack_name.trim().length === 0
+  ) {
+    return res
+      .status(400)
+      .json({
+        error:
+          "slack_name parameter is required and must be a non-empty string",
+      });
+  }
+  if (!track || typeof track !== "string" || track.trim().length === 0) {
+    return res
+      .status(400)
+      .json({
+        error: "track parameter is required and must be a non-empty string",
+      });
+  }
+
   // Get the current day and UTC time
   const currentDate = new Date();
   const currentDay = currentDate.toLocaleDateString("en-US", {
@@ -18,16 +39,21 @@ app.get("/api", (req: Request, res: Response) => {
 
   // Construct the response JSON
   const responseJson = {
-    slack_name,
+    slack_name: slack_name.trim(),
     current_day: currentDay,
     utc_time: utcTime,
-    track,
+    track: track.trim(),
     github_file_url: githubFileUrl,
     github_repo_url: githubRepoUrl,
     status_code: 200,
   };
 
   res.json(responseJson);
+});
+
+//  Running the server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
 
 export default app;
